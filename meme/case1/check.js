7230499b15c6d2bf2a0716ed8423259fab755d9a
nze <denkler@gmail.com>
2020-02-11 11:16:50 +0100

    updated meme generator

--- meme/case1/check.js
+++ meme/case1/check.js
@@ -3,13 +3,13 @@
     const testCase = new TestCase("Memes");
     const screen = new Region();
     const env = new Environment();
-    env.setSimilarity(0.82);
     const max = 20000;
-    const textEditor = new Application("/Applications/TextMate.app/Contents/Resources/mate");
+    const url = "https://imgflip.com/memegenerator/74454246/Beach-Testing-3";
 
     async function waitForElement(element, timeout) {
         await _wait(timeout, () => _isVisible(element));
     }
+
     async function createMeme(top, bottom) {
         await waitForElement(_textarea("mm-text"), max);
 
@@ -18,8 +18,10 @@
         await _setValue(textFields[1], bottom);
 
         await _click(_div("Generate"));
+        await testCase.endOfStep("Created Meme " + top + " " + bottom);
 
         await waitForElement(_textbox("img-code link"), max);
+        await _highlight(_textbox("img-code link"), 200);
         env.setClipboard(await _getValue(_textbox("img-code link")));
 
     }
@@ -27,8 +29,8 @@
         let editorButtonReg
         let editorReg
         let chromeButtonReg
-        editorButtonReg = screen.find("textmate.png");
-        await _wait(max, () => editorButtonReg.mouseMove().click());
+        editorButtonReg = await screen.find("textmate.png");
+        await _wait(max, () => editorButtonReg.click());
         editorReg = env.getRegionFromFocusedWindow();
         await _wait(max, () => editorReg.move(20, 5).click());
         await env.keyDown(Key.CMD, Key.DOWN);
@@ -38,6 +40,8 @@
         await env.type(Key.ENTER);
 
         await env.pasteClipboard();
+        await testCase.endOfStep("Saved Meme Link");
+
         chromeButtonReg = screen.find("chrome.png");
         await _wait(max, () => chromeButtonReg.mouseMove().click());
         await _wait(300);
@@ -46,182 +50,25 @@
 
     }
     try {
-        await _navigateTo("https://imgflip.com/memegenerator/74454246/Beach-Testing-3");
+        await _navigateTo(url);
+        await testCase.endOfStep("Navigate to url");
+        env.setSimilarity(0.82);
 
-        await createMeme("Franzi", "HomeOffice");
+        await createMeme("My Girlfriend", "Doing HomeOffice");
         await saveMemeUrl();
-        await createMeme("Katzi", "Im Element");
+        await createMeme("Simon", "Developing Sakuli");
+        await saveMemeUrl();
+        await createMeme("DevDay", "at consol");
         await saveMemeUrl();
 
 
-
-
-        // await _wait(3000);
-        
-        // await _wait(5000);
-        // let navEl = _listItem("nav-item dropdown nav-item-201");
-        // await highlightClick(navEl);
-        // await highlightClick(_listItem(0, _in(navEl)));
-        // await _highlight(_byXPath('//img[@title="Sakuli"]'));
-        // await _highlight(_heading2(/Zur Sakuli Produktseite/));
-        // //await _focus(_div("card card-type1"))
-        // await _highlight(_div("card card-type1[0]"));
-        // await _highlight(_div("card card-type1[1]"));
-        // await _highlight(_div("card card-type1[2]"));
-        // await highlightClick(_heading3("10-Schritte-Guide für End-to-End-Monitoring"));
-
-        // await _navigateTo("https://sakuli.io/wp");
-
-        // const mainWindow = driver.getWindowHandle();
-        // const tabs = driver.getAllWindowHandles();
-        // driver.switchTo().window(mainWindow);
-
-        // await highlightClick(_button("I agree"));
-        // //await highlightClick(_italic("fas fa-download"));
-        // //todo check for file on file system in container!
-        // await highlightClick(_link(/Contact/));
-        // await _setSelected(_select("powermail_field_salutation"), "Mrs.");
-        // await _setValue(_textbox("powermail_field_firstname"), "Henriette");
-        // await _setValue(_textbox("powermail_field_name"), "Kette");
-
-
-        // await _setValue(_emailbox("powermail_field_email"), "h.kette@thisIsNotADomain.wuxi");
-        // await _focus(_textbox("powermail_field_phone"));
-        // await env.type("089 / 3993 555 98112");
-
-
-        // await _setValue(_textbox("powermail_field_company"), "Consol GmbH");
-        // await _setValue(_textarea("powermail_field_mitteilung"), "Hallo, ich würde gerne mehr über Ihr Produkt erfahren, bitte kontaktieren Sie mich. H.Kette");
-
-        // await _check(_checkbox("powermail_field_datenschutz_1"));
-        // await _highlight(_button("Send"));
-        // await _click(_button("× Close"));
-        // //*[@id="c3616"]/div/div[1]/div/div/figure/div/picture/img
-        // // await highlightClick(_link(/E2E Testing/));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if (await _exists(_div("gdpr-info-box")) && await _exists(_link("Learn more", _in(_div("gdpr-info-box"))))) {
-        //     await highlight(_div("gdpr-info-box"));
-        //     await focusClick(_link("Learn more", _in(_div("gdpr-info-box"))));
-        //     if (await _exists(_div("csc-textpic-text"))){
-        //         await highlight(_div("csc-textpic-text"));
-        //         await testCase.endOfStep("GDPR Content is visible");
-        //     }else{
-        //         await testCase.endOfStep("GDPR Content couldn´t be fetched or rendered");
-        //     }
-        //     await focusClick(_button("Close"));
-        //     await highlight(_button("I agree"));
-        //     await focusClick(_button("I agree"));
-        //     await testCase.endOfStep("Accepted Cookies");
-        // }else{
-        //     await testCase.endOfStep("Cookie banner did not show up!");
-        // }
-        // await _highlight(_div("hero__image"),6000);
-        // await highlight(_link("Legal Notice"));
-        // await focusClick(_link("Legal Notice"));
-        // if (await _exists(_heading2("address"))){
-        //     await highlight(_heading2("address"));
-        //     await testCase.endOfStep("Legal notice content fetched and displayed.");
-        // }else{
-        //     await testCase.endOfStep("Legal notice content couldn´t be fetched or rendered.");
-        // }
-        // await _navigateTo(url);
-        // await screen.mouseWheelUp(10);
-        // await screen.find("getting.png").then(target=>target.click());
-        // await testCase.endOfStep("Getting Started opened.")
-        // await highlight(_code("npm init"));
-        // await testCase.endOfStep("Code component found.")
-        // await _navigateTo(url);
-        // await highlight(_link("button[1]"));
-        // await focusClick(_link("button[1]"));
-        // await highlight(_heading1("Testing"));
-        // await testCase.endOfStep("Opened Testing page.");
-        // await _navigateTo(url);
-        // await highlight(_link("button[2]"));
-        // await focusClick(_link("button[2]"));
-        // await highlight(_heading1(/Monitoring/));
-        // await testCase.endOfStep("Opened Monitoring page.");
-        // await _navigateTo(url);
-        // await highlight(_link("button[3]"));
-        // await focusClick(_link("button[3]"));
-        // await highlight(_heading1(/RPA/));
-        // await testCase.endOfStep("Opened RPA page.");
-
-        // await _navigateTo(url + "/enterprise");
-        // await testCase.endOfStep("Opened Enterprise page.");
-        // // if (await _exists(_link("Learn more"))) {
-        // //     await highlight(_link("Learn more"));
-        // //     await testCase.endOfStep("Cookie Banner found");
-        // //     await highlight(_button("I agree"));
-        // //     await focusClick(_button("I agree"));
-        // // }else{
-        // //     await testCase.endOfStep("No Cookie Banner found.");
-        // // }
-        // let links = await _collect("_link",/Request/);
-        // await highlight(_list("price-table"));
-        // await highlight(links[1]);
-        // await focusClick(links[1]);
-        // await highlight(_div("checkbox "));
-        // await testCase.endOfStep("Contact form fetched and displayed.");
-        // await _setSelected(_select("powermail_field_salutation"), "Mrs.");
-        // await _setValue(_textbox("powermail_field_firstname"), "Test Vorname");
-        // await _setValue(_textbox("powermail_field_name"), "Test Nachname");
-        // await _setValue(_emailbox("powermail_field_email"), "Test@testuseryxyxyxyx.com");
-        // await _setValue(_textbox("powermail_field_company"), "Test Company");
-        // await _setValue(_textarea("powermail_field_mitteilung"), "Hello, this is a test message. Kind regards, The Sakuli.io Bot.");
-        // await testCase.endOfStep("Contact form filled.");
-        // await highlight(_button(/Close/));
-        // await _click(_button(/Close/));
-        // await testCase.endOfStep("Closed Contact Form.");
-
-        // // await _wait(2000);
-        // // //await env.type("P", Key.CMD);
-        // // await env.keyDown(Key.CMD, Key.P);
-        // // await env.keyUp(Key.CMD, Key.P);
-        // // await _wait(5000);
-
-        // // await screen.find("print_target_black.png").then(target=>target.click());
-
-        // // await screen.find("print_as_pdf.png").then(target=>target.click());
-        await _wait(4000);
+        await _wait(2000);
 
     } catch (e) {
         await testCase.handleException(e);
     } finally {
         await testCase.saveResult();
     }
-})().then(done); 
+})(); 
 
 
