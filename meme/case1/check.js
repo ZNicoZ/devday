c5cb8b5f4517bfde61d3820092044e58d4962020
Nico Zellner <nico.zellner@consol.de>
2020-03-02 13:01:10 +0100

    hightlight to meme case for demo purposes

--- meme/case1/check.js
+++ meme/case1/check.js
@@ -12,18 +12,16 @@
 
     async function createMeme(top, bottom) {
         await waitForElement(_textarea("mm-text"), max);
-
         const textFields = await _collect("_textarea", "mm-text");
+        await _highlight(textFields[0], 500);
         await _setValue(textFields[0], top);
+        await _highlight(textFields[1], 500);
         await _setValue(textFields[1], bottom);
-
         await _click(_div("Generate"));
         await testCase.endOfStep("Created Meme " + top + " " + bottom);
-
         await waitForElement(_textbox("img-code link"), max);
-        await _highlight(_textbox("img-code link"), 200);
+        await _highlight(_textbox("img-code link"), 500);
         env.setClipboard(await _getValue(_textbox("img-code link")));
-
     }
     async function saveMemeUrl() {
         let editorButtonReg
@@ -45,19 +43,22 @@
         await _wait(max, () => chromeButtonReg.mouseMove().click());
         await _wait(300);
 
+
+        await _highlight(_div("Make another"));
         await _click(_div("Make another"));
 
     }
     try {
         await _navigateTo(url);
+        await _highlight(_heading1(/.*/));
         await testCase.endOfStep("Navigate to url");
         env.setSimilarity(0.82);
 
         await createMeme("Simon", "Developing Sakuli");
         await saveMemeUrl();
-        await createMeme("DevDay", "at consol");
+        await createMeme("Sakuli", "Doing the Work");
         await saveMemeUrl();
-        await createMeme("When I hear", "Home-Office");
+        await createMeme("Selling Sakuli", "Be Like");
         await saveMemeUrl();
 
     } catch (e) {
